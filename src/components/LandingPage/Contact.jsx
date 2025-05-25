5c8f218b7b72e7585a3eba0420d4e97d8b29a3e2
elyme-12 <nylase12@gmail.com>
2025-05-25 11:53:00 +0800

    Update Contact.jsx

--- src/components/LandingPage/Contact.jsx
+++ src/components/LandingPage/Contact.jsx
@@ -15,14 +15,14 @@ function Contact(){
                     <a href="https://myaccount.google.com/" className="contact-link">
                         <img src={email} alt="Gmail Logo" />
                     </a>
-                    <a href="https://www.facebook.com/margaretteann.estanol" className="contact-link" target="_blank" rel="noopener noreferrer">
+                    <a href="https://www.facebook.com/elysalyn.barlan.9" className="contact-link" target="_blank" rel="noopener noreferrer">
                         <img src={facebook} alt="Facebook Logo" />
                     </a>
-                    <a href="https://www.tiktok.com/@margarette12_" className="contact-link" target="_blank" rel="noopener noreferrer">
+                    <a href="https://www.tiktok.com/@kyoomin_12" className="contact-link" target="_blank" rel="noopener noreferrer">
                         <img src={tiktok} alt="Tiktok Logo" />
                     </a>
-                    <a href="https://www.instagram.com/margaretteannf/" className="contact-link" target="_blank" rel="noopener noreferrer">
-                        <img src={telegram} alt="Instagram Logo" />
+                    <a href="https://t.me/elimint24" className="contact-link" target="_blank" rel="noopener noreferrer">
+                        <img src={telegram} alt="Telegram Logo" />
                     
                     </a>
                 </div>
